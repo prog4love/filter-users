5a1adff72d61fbfecc698118283b5daf8f16b297
prog4love <prog4love@users.noreply.github.com>
2018-11-04 21:25:25 +0200

    Upgrade user filtering selector functions

--- src/selectors.js
+++ src/selectors.js
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect';
-import { findChunks } from 'highlight-words-core';
+import { findPhoneMatchingChunks } from './utils';
 
 const QUERY_LENGTH_LIMIT = 30;
 
@@ -9,7 +9,21 @@ export const getSearchQuery = state => state.searchQuery;
 
 export const getFilteredUsers = createSelector(
   [getUsers, getSearchQuery],
-  filterUsers,
+  (users, searchQuery) => {
+    // NOTE: have been already trimmed and lowercased after input
+    const query = searchQuery.trim();
+
+    if (query.length > QUERY_LENGTH_LIMIT) {
+      console.warn('Search query string length limit was exceeded');
+      return users;
+    }
+    if (query === '') {
+      return users;
+    }
+    return users.filter((user) => {
+      return findMatchInUserData(user, query);
+    });
+  },
 );
 
 export const getActiveUser = createSelector(
@@ -25,188 +39,48 @@ export const getActiveUser = createSelector(
   },
 );
 
-// function findMatchingChunks({
-//   autoEscape,
-//   caseSensitive,
-//   sanitize,
-//   searchWords,
-//   textToHighlight
-// }) {
-//   textToHighlight = sanitize(textToHighlight)
-//
-//   return searchWords
-//     .filter(searchWord => searchWord) // Remove empty words
-//     .reduce((chunks, searchWord) => {
-//       const regex = new RegExp(searchWord, caseSensitive ? 'g' : 'gi')
-//
-//       let match
-//       while ((match = regex.exec(textToHighlight))) {
-//         let start = match.index
-//         let end = match.lastIndex
-//         // We do not return zero-length matches
-//         if (end > start) {
-//           chunks.push({start, end})
-//         }
-//       }
-//       return chunks
-//     }, [])
-// }
-
-// it also used as custom findChunks function for Highlighter component
-export function findPhoneMatchingChunks({ searchWords, textToHighlight }) {
-  if (
-    process.env.NODE_ENV !== 'production'
-    && (
-    typeof textToHighlight !== 'string'
-    || !Array.isArray(searchWords)
-    || typeof searchWords[0] !== 'string'
-  )) {
-    throw new Error('Expected array of strings and string params');
+export const comparePropWithQuery = (propName, propValue, query) => {
+  const value = propValue.toLowerCase();
+  // console.log(`COMPARE: "${query}" === "${value}" (${propName})`);
+  if (propName !== 'phone') {
+    return value.includes(query);
   }
-  const chunks = findChunks({
-    autoEscape: true,
-    caseSensitive: false,
-    searchWords,
-    textToHighlight,
-  });
-  const [query] = searchWords;
-  const phoneString = textToHighlight;
+  return findPhoneMatchingChunks({
+    searchWords: [query],
+    textToHighlight: value,
+  }).length > 0;
+};
 
-  if (chunks.length > 0) {
-    console.log('MATCHING CHUNKS: ', chunks);
-  }
-  if (!query || !textToHighlight) {
-    return chunks;
-  }
+export function findMatchInUserData(userData, searchQuery) {
+  // NOTE: remember to NOT mutate initial userData objects further
+  const query = searchQuery.toLowerCase();
+  let hasMatch = false;
 
-  // const isNumeric = /^[0-9]*$/.test(query);
-  // discard everything except numbers
-  // const queryNums = query.replace(/\D/g, '');
-
-  function collectNumericCharIndexes(originalText) {
-    return originalText.split('').reduce((collection, char, index) => {
-      if (/[0-9]/.test(char)) {
-        collection.indexes.push(index);
-        collection.str += char;
-      }
-      return collection;
-    }, { indexes: [], str: '' });
-  }
-
-  // TODO: add max distance between match chunks
-
-  const phoneNums = collectNumericCharIndexes(phoneString);
-  const queryNums = query.replace(/\D/g, '');
-  const startIndex = phoneNums.str.indexOf(queryNums);
-
-  if (startIndex < 0) {
-    return chunks;
-  }
-  const matchIndexes = phoneNums.indexes.slice(startIndex, startIndex + queryNums.length);
-
-  const numChunks = matchIndexes.reduce((chunks, current, i) => {
-    if (i === 0) {
-      chunks.push({ start: current });
+  Object.keys(userData).forEach((field) => {
+    if (hasMatch) {
+      return;
     }
-    const next = matchIndexes[i + 1];
-    const isLast = i === matchIndexes.length - 1;
-
-    if (isLast) {
-      // [..., { start: ..., end: current + 1 }]
-      chunks[chunks.length - 1].end = current + 1;
-      return chunks;
+    const fieldProps = userData[field];
+    // special case - "id" field
+    if (field === 'id') {
+      return;
     }
-    // e.g. current: 4, next: 7
-    if (current + 1 !== next) {
-      // [..., { start: ..., end: current + 1 }]
-      chunks[chunks.length - 1].end = current + 1;
-      // create new chunk
-      chunks.push({ start: next  });
-    }
-    return chunks;
-  }, []);
-
-  console.log('NUM CHUNKS: ', numChunks);
-
-  return chunks;
-}
-
-export function filterUsers(users, searchQuery) {
-  // NOTE: remember to NOT mutate initial users array and user objects further
-  const compareToQuery = (propValue, query, propName) => {
-    const value = propValue.toLowerCase();
-
-    // console.log(`COMPARE: "${query}" === "${value}" (${propName})`);
-
-    if (propName !== 'phone') {
-      return value.includes(query);
-    }
-    return findPhoneMatchingChunks({
-      searchWords: [query],
-      textToHighlight: value,
-    }).length > 0;
-  }
-  // NOTE: have been already trimmed and lowercased after input
-  searchQuery = searchQuery.trim();
-
-  if (searchQuery.length > QUERY_LENGTH_LIMIT) {
-    console.warn('Search query string length limit was exceeded');
-    return users;
-  }
-  if (searchQuery === '') {
-    return users;
-  }
-  const filteredUsers = users.filter((user) => {
-    // address:
-    //   city: "New Devon"
-    //   country: "Guinea-Bissau"
-    //   street: "1520 Zemlak Cove"
-    //   zipCode: "42586-7898"
-
-    // contact:
-    //   email: "Gerry_Hackett77@gmail.com"
-    //   phone: "(895) 984-0132"
-
-    // general:
-    //   avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/kevinoh/128.jpg"
-    //   firstName: "Liana"
-    //   lastName: "Crooks"
-
-    // job:
-    //   company: "Ledner, Johnson and Predovic"
-    //   title: "Investor Functionality Coordinator"
-    let hasMatch = false;
-    searchQuery = searchQuery.toLowerCase();
-
-    // compare needed properties to search query
-    Object.keys(user).forEach((field) => {
+    Object.keys(fieldProps).forEach((prop) => {
       if (hasMatch) {
         return;
       }
-      const fieldProps = user[field];
-      // special case - "id" field
-      if (field === 'id') {
+      const value = fieldProps[prop];
+
+      // TODO: ensure that value is string
+      if (prop === 'avatar' || !value) {
         return;
       }
-      Object.keys(fieldProps).forEach((prop) => {
-        if (hasMatch) {
-          return;
-        }
-        const value = fieldProps[prop];
-
-        // TODO: ensure that value is string
-        if (prop === 'avatar' || !value) {
-          return;
-        }
-        hasMatch = compareToQuery(value, searchQuery, prop);
-        // TEMP:
-        if (hasMatch) {
-          console.log(`TRUE: "${searchQuery}" === "${fieldProps[prop]}" (${prop})`);
-        }
-      });
+      hasMatch = comparePropWithQuery(prop, value, query);
+      // TEMP:
+      if (hasMatch) {
+        console.log(`TRUE: "${query}" === "${value}" (${prop})`);
+      }
     });
-    return hasMatch;
   });
-
-  return filteredUsers;
+  return hasMatch;
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { findChunks } from 'highlight-words-core';
+
 export const concatClassNames = (addClassName, ...classNames) => {
 	return classNames.concat(addClassName).join(' ');
 }
@@ -15,4 +17,108 @@ export const copy = (source) => {
   } else {
     return Object.assign({}, source);
   }
+}
+
+function collectNumericCharIndexes(originalText) {
+  return originalText.split('').reduce((collection, char, index) => {
+    if (/[0-9]/.test(char)) {
+      collection.indexes.push(index);
+      collection.str += char;
+    }
+    return collection;
+  }, { indexes: [], str: '' });
+}
+
+function checkDistanceBetweenNumChars(maxDistance = 3, originalText) {
+  let suitLimit = true;
+
+  originalText.split('').reduce((prevIndex, char, index) => {
+    if (!suitLimit) {
+      return prevIndex;
+    }
+    if (/[0-9]/.test(char)) {
+      suitLimit = prevIndex === null
+        ? suitLimit
+        : prevIndex + maxDistance >= index;
+      return index;
+    } else {
+      return prevIndex;
+    }
+  }, null);
+
+  return suitLimit;
+}
+
+// used as custom findChunks function for Highlighter component
+// and as helper function in users filtering selector
+export function findPhoneMatchingChunks({ searchWords, textToHighlight }) {
+  if (
+    process.env.NODE_ENV !== 'production'
+    && (
+    typeof textToHighlight !== 'string'
+    || !Array.isArray(searchWords)
+    || typeof searchWords[0] !== 'string'
+  )) {
+    throw new Error('Expected array of strings and string params');
+  }
+  const chunks = findChunks({
+    autoEscape: true,
+    caseSensitive: false,
+    searchWords,
+    textToHighlight,
+  });
+  const [query] = searchWords;
+  const phoneString = textToHighlight;
+  // TEMP:
+  if (chunks.length > 0) {
+    console.log('MATCHING CHUNKS: ', chunks);
+  }
+  if (!query || !textToHighlight) {
+    return chunks;
+  }
+  // const isNumeric = /^[0-9]*$/.test(query);
+  // discard everything except numbers
+  // const queryNums = query.replace(/\D/g, '');
+
+  // TODO: add max distance limit between num char indexes in query
+  const queryNums = query.replace(/\D/g, '');
+
+  if (queryNums === '' || !checkDistanceBetweenNumChars(2, query)) {
+    return chunks;
+  }
+  const phoneNums = collectNumericCharIndexes(phoneString);
+  const startIndex = phoneNums.str.indexOf(queryNums);
+
+  if (startIndex < 0) {
+    return chunks;
+  }
+  const matchIndexes = phoneNums.indexes.slice(startIndex, startIndex + queryNums.length);
+
+  // generate array of matching numeric chunks { start: Number, end: Number },
+  // where start and end - indexes of chars in original phone string
+  const numChunks = matchIndexes.reduce((chunks, current, i) => {
+    if (i === 0) {
+      chunks.push({ start: current });
+    }
+    const next = matchIndexes[i + 1];
+    const isLast = i === matchIndexes.length - 1;
+
+    if (isLast) {
+      // [..., { start: ..., end: current + 1 }]
+      chunks[chunks.length - 1].end = current + 1;
+      return chunks;
+    }
+    // e.g. current: 4, next: 7
+    if (current + 1 !== next) {
+      // [..., { start: ..., end: current + 1 }]
+      chunks[chunks.length - 1].end = current + 1;
+      // create new chunk
+      chunks.push({ start: next  });
+    }
+    return chunks;
+  }, []);
+
+  console.log('NUM CHUNKS: ', numChunks);
+
+  return chunks;
 }
